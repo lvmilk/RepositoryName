1c8c37b1f0634d48e1870cd8e16ca6c9201980dc
victor <victor@dell>
2015-09-03 17:20:54 +0800

    haha

---
+++ MAS-ejb/src/java/EntityHandler/FlightHandler.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package EntityHandler;
+
+/**
+ *haha
+ * @author victor
+ */
+public interface FlightHandler {
+    
+}
