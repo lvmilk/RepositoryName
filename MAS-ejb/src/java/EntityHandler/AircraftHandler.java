ecd3562e5b954c9aed7f309fb73996cede9f5cb8
victor <victor@dell>
2015-09-05 17:31:21 +0800

    no bug

--- MAS-ejb/src/java/EntityHandler/AircraftHandler.java
+++ MAS-ejb/src/java/EntityHandler/AircraftHandler.java
@@ -2,6 +2,7 @@ package EntityHandler;
 
 import Entity.Aircraft;
 import Entity.AircraftType;
+import Exception.MASException;
 import java.util.ArrayList;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -16,22 +17,22 @@ public class AircraftHandler implements AircraftHandlerInterface{
 
     @Override
     public void addAircraftType(AircraftType at){
-        
+
     }
 
     @Override
     public boolean deleteAircraftType(AircraftType at){
-        
+        return true;
     }
 
     @Override
     public ArrayList<AircraftType> findAircraftType(String type){
-        
+        return null;
     }
 
     @Override
     public boolean hasAircraftType(String type){
-        
+        return false;
     }
 
     @Override
@@ -41,16 +42,46 @@ public class AircraftHandler implements AircraftHandlerInterface{
 
     @Override
     public boolean deleteAircraft(Aircraft af){
-        
+        return false;
     }
 
     @Override
     public ArrayList<AircraftType> findAircraft(String registrationNumber){
-        
+        return null;
     }
 
     @Override
     public boolean hasAircraft(String registrationNumber){
-        
+        return false;
+    }
+
+    @Override
+    public void updateAircraftType(AircraftType at) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public AircraftType findAircraftType(long id) throws MASException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<AircraftType> listAircraftType() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateAircraft(Aircraft af) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Aircraft findAircraft(long id) throws MASException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<AircraftType> listAircraft() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
