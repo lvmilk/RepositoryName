7569a0f2ff58832d1e7da7d8e46671990a595d11
Xu <Xu@Xu-PC>
2015-09-19 09:48:18 +0800

    Commit_Xu Bohan_Route Planning_v1

---
+++ MAS-ejb/src/java/Entity/APS/Route.java
@@ -0,0 +1,158 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity.APS;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author Xu
+ */
+@Entity
+public class Route implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String originIATA;
+    private String destIATA;
+    private Double distance;
+    private Double basicFcFare;
+    private Double basicBcFare;
+    private Double basicPecFare;
+    private Double basicEcFare;
+    // havenot added aircraft type yet
+    
+    @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy="route")
+    private Airport origin;
+    @OneToMany(cascade = {CascadeType.PERSIST}, mappedBy="route")
+    private Airport dest;
+
+    public void create(String originIATA, String destIATA, Double distance, Double basicFcFare, Double basicBcFare, Double basicPecFare, Double basicEcFare) {
+        this.originIATA = originIATA;
+        this.destIATA = destIATA;
+        this.distance = distance;
+        this.basicFcFare = basicFcFare;
+        this.basicBcFare = basicBcFare;
+        this.basicPecFare = basicPecFare;
+        this.basicEcFare = basicEcFare;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getOriginIATA() {
+        return originIATA;
+    }
+
+    public void setOriginIATA(String originIATA) {
+        this.originIATA = originIATA;
+    }
+
+    public String getDestIATA() {
+        return destIATA;
+    }
+
+    public void setDestIATA(String destIATA) {
+        this.destIATA = destIATA;
+    }
+
+    public Double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Double distance) {
+        this.distance = distance;
+    }
+
+    public Double getBasicFcFare() {
+        return basicFcFare;
+    }
+
+    public void setBasicFcFare(Double basicFcFare) {
+        this.basicFcFare = basicFcFare;
+    }
+
+    public Double getBasicBcFare() {
+        return basicBcFare;
+    }
+
+    public void setBasicBcFare(Double basicBcFare) {
+        this.basicBcFare = basicBcFare;
+    }
+
+    public Double getBasicPecFare() {
+        return basicPecFare;
+    }
+
+    public void setBasicPecFare(Double basicPecFare) {
+        this.basicPecFare = basicPecFare;
+    }
+
+    public Double getBasicEcFare() {
+        return basicEcFare;
+    }
+
+    public void setBasicEcFare(Double basicEcFare) {
+        this.basicEcFare = basicEcFare;
+    }
+
+    public Airport getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(Airport origin) {
+        this.origin = origin;
+    }
+
+    public Airport getDest() {
+        return dest;
+    }
+
+    public void setDest(Airport dest) {
+        this.dest = dest;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Route)) {
+            return false;
+        }
+        Route other = (Route) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Entity.APS.Route[ id=" + id + " ]";
+    }
+
+}
---
+++ MAS-ejb/src/java/SessionBean/APS/RoutePlanningBeanLocal.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SessionBean.APS;
+
+import Entity.APS.Airport;
+import Entity.APS.Route;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Xu
+ */
+@Local
+public interface RoutePlanningBeanLocal {
+
+    public void addAirport(String IATA, String airportName, String cityName, String countryCode, String timeZone) throws Exception;
+
+    public void editAirport(String IATA, String airportName, String cityName, String countryCode, String timeZone) throws Exception;
+
+    public void deleteAirport(String IATA) throws Exception;
+
+    public Airport viewAirport(String IATA) throws Exception;
+
+    public List<Airport> viewAllAirport();
+
+    public void addRoute(String originIATA, String destIATA, Double distance, Double basicFcFare, Double basicBcFare, Double basicPecFare, Double basicEcFare) throws Exception;
+
+    public void editRouteFare(String originIATA, String destIATA, Double basicFcFare, Double basicBcFare, Double basicPecFare, Double basicEcFare) throws Exception;
+
+    public void deleteRoute(String originIATA, String destIATA) throws Exception;
+
+    public List<Route> viewAllRoute();
+
+    public Route viewRoute(String originIATA, String destIATA) throws Exception;
+
+}
