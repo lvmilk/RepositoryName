85911fbcf76a630aa4af070c90386fa006e894f3
LI HAO <LI HAO@172.25.106.5>
2015-09-22 15:57:02 +0800

    Able to create admin account

--- MAS-war/src/java/filters/AuthFilter.java
+++ MAS-war/src/java/filters/AuthFilter.java
@@ -46,7 +46,7 @@ public class AuthFilter implements Filter {
                                        || reqURI.indexOf("/public/") >= 0 || reqURI.contains("javax.faces.resource")|| reqURI.indexOf("/staffMain.xhtml") >= 0 )
                    chain.doFilter(request, response);
             else   // user didn't log in but asking for a page that is not allowed so take user to login page
-                   res.sendRedirect(req.getContextPath() + "/login.xhtml");  // Anonymous user. Redirect to login page
+                   res.sendRedirect(req.getContextPath() + "/staffMain.xhtml");  // Anonymous user. Redirect to login page
       }
      catch(Throwable t) {
          System.out.println( t.getMessage());
