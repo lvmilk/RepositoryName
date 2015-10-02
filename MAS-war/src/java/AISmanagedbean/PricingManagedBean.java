2bde5f97c384231d9f529e3122568e6feeda733e
christine1993 <snowky1993@gmail.com>
2015-10-02 15:39:44 +0800

    Merge origin/master
    Conflicts:
    	MAS-ejb/src/java/Entity/APS/AircraftType.java

--- MAS-war/src/java/AISmanagedbean/PricingManagedBean.java
+++ MAS-war/src/java/AISmanagedbean/PricingManagedBean.java
@@ -15,8 +15,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.ejb.EJB;
-import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
