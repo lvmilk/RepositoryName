6f8f150a9e783c5c4dd04abb8e046b0933586f17
LI HAO <LI HAO@172.23.194.209>
2015-11-14 16:15:23 +0800

    Bug fixed

--- MAS-war/src/java/ADSmanagedbean/UpdateReservationManagedBean.java
+++ MAS-war/src/java/ADSmanagedbean/UpdateReservationManagedBean.java
@@ -65,6 +65,9 @@ public class UpdateReservationManagedBean implements Serializable {
     private Map<FlightInstance, BookingClassInstance> flightToBkInstance = new HashMap<>();
 
     private FlightInstance selectedFlight;
+    
+    private String bkSystem;
+    private String companyName;
 
     public UpdateReservationManagedBean() {
     }
@@ -77,8 +80,8 @@ public class UpdateReservationManagedBean implements Serializable {
 
         selectedPsg = (Passenger) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("selectedPsg");
         selectedPsgList = (List<Passenger>) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("PsgList");
-        bkSystem = (String) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("bkSystem");
-        companyName = (String) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("companyName");
+        bkSystem=(String) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("bkSystem");
+        companyName=(String) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("companyName");
 
         rsvList = mr.getCompanyReservations(companyName);
         selectedRsv = (Reservation) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("selectedRsv");
@@ -413,6 +416,34 @@ public class UpdateReservationManagedBean implements Serializable {
 
     public void setSelectedFlight(FlightInstance selectedFlight) {
         this.selectedFlight = selectedFlight;
+    }
+
+    /**
+     * @return the bkSystem
+     */
+    public String getBkSystem() {
+        return bkSystem;
+    }
+
+    /**
+     * @param bkSystem the bkSystem to set
+     */
+    public void setBkSystem(String bkSystem) {
+        this.bkSystem = bkSystem;
+    }
+
+    /**
+     * @return the companyName
+     */
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    /**
+     * @param companyName the companyName to set
+     */
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
     }
 
 }
