a5b1ef150cd7fea5f18b6546b7295d0069fec28b
LI HAO <LI HAO@LIHAO-PC>
2015-11-18 01:19:43 +0800

    setup airline and gds flight entity

--- MAS-ejb/src/java/Entity/GDS/Airline.java
+++ MAS-ejb/src/java/Entity/GDS/Airline.java
@@ -9,6 +9,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -27,8 +28,11 @@ public class Airline implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(unique=true)
     private String email;
+    @Column(unique=true)
     private String name;
+    @Column(unique=true)
     private String IATA;
 
     @OneToMany(cascade = {CascadeType.PERSIST},mappedBy = "airline")
