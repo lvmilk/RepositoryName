788768747e0016bbcb0074245f6e77f8dd900590
LI HAO <LI HAO@LIHAO-PC>
2015-11-21 18:19:17 +0800

    completed unit test for routeplanningBean

--- MAS-ejb/test/testSessionBean/APS/RoutePlannigBeanRemoteTest.java
+++ MAS-ejb/test/testSessionBean/APS/RoutePlannigBeanRemoteTest.java
@@ -5,6 +5,7 @@
  */
 package testSessionBean.APS;
 
+import Entity.APS.AircraftType;
 import Entity.APS.Airport;
 import Entity.APS.Route;
 import SessionBean.APS.RoutePlanningBeanRemote;
@@ -100,16 +101,211 @@ public class RoutePlannigBeanRemoteTest {
         Route rt = new Route();
         rt = rpbr.viewRoute("SIN", "XIA");
         assertEquals("SIN", rt.getOrigin().getIATA());
-
     }
 
-    @Test(expected=Exception.class)
+    @Test(expected = Exception.class)
     public void test06ViewRoute_NoSuchRoute() throws Exception {
         System.out.println("test06ViewRoute_NoSuchRoute");
         Route rt = new Route();
         rt = rpbr.viewRoute("SIN", "NRT");
         assertEquals("SIN", rt.getOrigin().getIATA());
 
+    }
+
+    @Test
+    public void test07ViewAsOriginRoute() throws Exception {
+        System.out.println("test07ViewAsOriginRoute");
+        Airport ap = new Airport();
+        ap = rpbr.findAirport("SIN");
+
+        List<Route> rt = new ArrayList<Route>();
+        rt = rpbr.viewApAsOriginRoute(ap);
+        assertFalse(rt.isEmpty());
+    }
+
+    @Test
+    public void test08ViewAsDestRoute() throws Exception {
+        System.out.println("test08ViewAsDestRoutes");
+        Airport ap = new Airport();
+        ap = rpbr.findAirport("SIN");
+
+        List<Route> rt = new ArrayList<Route>();
+        rt = rpbr.viewApAsOriginRoute(ap);
+        assertFalse(rt.isEmpty());
+    }
+
+    @Test
+    public void test09ViewAllRouteString() throws Exception {
+        System.out.println("test09ViewAllRouteString");
+        List<String> sList = new ArrayList<String>();
+        sList = rpbr.viewAllRouteString();
+
+        assertFalse(sList.isEmpty());
+    }
+
+    @Test
+    public void test10CanDeleteAirportList() throws Exception {
+        System.out.println("test10CanDeleteAirportList");
+        List<Airport> ap = new ArrayList<Airport>();
+        ap = rpbr.canDeleteAirportList();
+
+        assertFalse(ap.isEmpty());
+    }
+
+    @Test
+    public void test11CannotDeleteAirportList() throws Exception {
+        System.out.println("test11CannotDeleteAirportList");
+        List<Airport> ap = new ArrayList<Airport>();
+        ap = rpbr.cannotDeleteAirportList();
+
+        assertFalse(ap.isEmpty());
+    }
+
+    @Test
+    public void test12FindRoute() throws Exception {
+        System.out.println("test12FindRoute");
+        Route rt = new Route();
+        rt = rpbr.findRoute(Long.valueOf(10000));
+
+        assertNotNull(rt);
+    }
+
+    @Test
+    public void test13CanDeleteRouteList() throws Exception {
+        System.out.println("test13CanDeleteRouteList");
+        List<Route> rt = new ArrayList<Route>();
+        rt = rpbr.canDeleteRouteList();
+
+        assertTrue(rt.isEmpty());
+    }
+
+    @Test
+    public void test14CheckFeasibleAcByDis() throws Exception {
+        System.out.println("test14CheckFeasibleAcByDis");
+        Route rt = new Route();
+        rt = rpbr.findRoute(Long.valueOf(20000));
+
+        List<AircraftType> atList = new ArrayList<AircraftType>();
+        atList = rpbr.checkFeasibleAcByDis(rt);
+        assertFalse(atList.isEmpty());
+    }
+
+    @Test
+    public void test16CheckFeasibleAcByAsp() throws Exception {
+        System.out.println("test16CheckFeasibleAcByAsp");
+        Route rt = new Route();
+        rt = rpbr.findRoute(Long.valueOf(20000));
+
+        List<AircraftType> atList = new ArrayList<AircraftType>();
+        atList = rpbr.checkFeasibleAcByAsp(rt);
+        assertFalse(atList.isEmpty());
+    }
+
+    @Test
+    public void test17FeasibleAc() throws Exception {
+        System.out.println("test17FeasibleAc");
+        Route rt = new Route();
+        rt = rpbr.findRoute(Long.valueOf(20000));
+
+        List<AircraftType> atList = new ArrayList<AircraftType>();
+        atList = rpbr.feasibleAc(rt);
+        assertFalse(atList.isEmpty());
+    }
+
+    @Test
+    public void test18FindAirport() throws Exception {
+        System.out.println("test18FindAirport");
+        Airport ap = new Airport();
+        ap = rpbr.findAirport("SIN");
+        assertNotNull(ap);
+
+    }
+
+    @Test
+    public void test19ViewHubAirport() throws Exception {
+        System.out.println("test19ViewHubAirport");
+        List<Airport> apList = new ArrayList<Airport>();
+        apList = rpbr.viewHubAirport();
+        assertFalse(apList.isEmpty());
+    }
+
+    @Test
+    public void test20IsHubAirport() throws Exception {
+        System.out.println("test20IsHubAirport");
+        boolean bl;
+        bl = rpbr.isHubAirport("SIN");
+        assertTrue(bl);
+
+    }
+
+    @Test
+    public void test21CalRouteDistance() throws Exception {
+        System.out.println("test21CalRouteDistance");
+        Double result;
+        result = rpbr.calRouteDistance("SIN", "XIA");
+        assertTrue(3700 < result);
+    }
+
+    @Test
+    public void test22CalRouteDistance_BoundryCase() throws Exception {
+        System.out.println("test22CalRouteDistance_ErrorCase");
+        Double result;
+        result = rpbr.calRouteDistance("SIN", "XIA");
+//        assertEquals((Double)3711.2,result);
+        assertTrue(3711.2 < result && result < 3711.22);
+
+    }
+
+    @Test
+    public void test23MaxBlockHour() throws Exception {
+        System.out.println("test23MaxBlockHour");
+        Double result;
+        result = rpbr.maxBlockHour(3800.0);
+        assertTrue(3.5 < result);
+    }
+
+    @Test
+    public void test24MaxBlockHour_ErrorCase() throws Exception {
+        System.out.println("test24MaxBlockHour_ErrorCase");
+        Double result;
+        result = rpbr.maxBlockHour(3800.0);
+        assertTrue(5.42<result && result<5.43);
+
+    }
+
+    @Test
+    public void test25MinBlockHour() throws Exception {
+        System.out.println("test25MinBlockHour");
+        Double result;
+        result = rpbr.minBlockHour(3800.0);
+        assertFalse(5.5 < result);
+    }
+
+    @Test
+    public void test26MinBlockHour_ErrorCase() throws Exception {
+        System.out.println("test26MinBlockHour_ErrorCase");
+        Double result;
+        result = rpbr.minBlockHour(3800.0);
+        assertEquals((Double) 3.8, result);
+    }
+
+    @Test
+    public void test27CanDeleteRoutePair() throws Exception {
+        System.out.println("test27CanDeleteRoutePair");
+        List<Route> rt=new ArrayList<Route>();
+        rt=rpbr.canDeleteRoutePair();
+        assertTrue(rt.isEmpty());
+    }
+
+    @Test
+    public void test28AirportHasFlight() throws Exception {
+        System.out.println("test28AirportHasFlight");
+        Airport ap = new Airport();
+        ap = rpbr.findAirport("SIN");
+        
+        boolean bl;
+        bl=rpbr.airportHasFlight(ap);
+        assertTrue(bl);
     }
 
     private RoutePlanningBeanRemote lookupRoutePlanningBeanRemote() {
