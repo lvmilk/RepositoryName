b4f4fcef05611a01ff56073dbe910a74a8867397
LI HAO <LI HAO@LIHAO-PC>
2015-11-22 20:35:12 +0800

    add testcases for AIS

--- MAS-ejb/src/java/SessionBean/AIS/ViewBookingClassPriceBean.java
+++ MAS-ejb/src/java/SessionBean/AIS/ViewBookingClassPriceBean.java
@@ -20,7 +20,7 @@ import javax.persistence.Query;
  * @author wang
  */
 @Stateful
-public class ViewBookingClassPriceBean implements ViewBookingClassPriceBeanLocal {
+public class ViewBookingClassPriceBean implements ViewBookingClassPriceBeanLocal,ViewBookingClassPriceBeanRemote {
 
     @PersistenceContext
     EntityManager em;
